dd4182df5e12518a6aca7c96cca9593079972849
Raffaele Superga <rs@tantosvago.it>
2020-01-07 01:24:01 +0100

    charts update

--- components/donut.js
+++ components/donut.js
@@ -6,27 +6,39 @@ var donutHoleRadius = totalRadius * 0.15
 var renderDonut = (type) => {
   console.log('we are in donut', data(type));
 
-  var color = d3.scaleOrdinal(d3.schemeCategory10);
+  // var color = d3.scaleOrdinal(d3.schemeCategory10);
+  var color = d3.scaleOrdinal().domain(data(type).values)
+    .range(data(type).colors)
 
   var svg = d3.select('#'+type).append('svg').attr('width', width).attr('height', height)
     .append('g')
     .attr('transform', `translate(${width / 2}, ${height / 2})`)
+  
 
   var arc = d3.arc().innerRadius(totalRadius - donutHoleRadius).outerRadius(totalRadius)
 
   var pie = d3.pie()
-    .value((d) => d.acres)
+    .value((d) => d.value)
     .sort(null)
 
   var path = svg
     .selectAll('path')
-    .data(pie(data(type)))
+    .data(pie(data(type).values))
     .enter()
     .append('path')
     .attr('d', arc)
-    .attr('fill', (d, i) => color(d.data.name))
+    .attr('fill', (d, i) => color(d.value))
 
-  var legendItemSize = 18
+  svg
+    .append('tspan')
+    .attr('x', 0)
+    .attr('dy', '1.3em')
+    .text((d, i) => {
+        return type.toUpperCase()
+        return data(type).total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')+'€'
+    })
+
+  /* var legendItemSize = 18
   var legendSpacing = 4
 
   var legend = svg
@@ -47,11 +59,11 @@ var renderDonut = (type) => {
     .append('rect')
     .attr('width', legendItemSize)
     .attr('height', legendItemSize)
-    .style('fill', color);
+    //.style('fill', color);
 
   legend
     .append('text')
     .attr('x', legendItemSize + legendSpacing)
     .attr('y', legendItemSize - legendSpacing)
-    .text((d) => d)
+    .text(type.toUpperCase()) */
 }
--- models/data.js
+++ models/data.js
@@ -1,5 +1,5 @@
 var data = (type) => {
-    return [{
+    /* return [{
         name: 'Washington',
         acres: 32205
       }, {
@@ -14,30 +14,43 @@ var data = (type) => {
       }, {
         name: 'Canada',
         acres: 257
-      }];
+      }]; */
     switch (type) {
         case 'revenue': {
             return {
                 id: 'revenue',
-                tablet: 120000,
-                smartphone: 80000,
-                total: 200000
+                total: 200000,
+                colors: ['#8EC353', '#4B6332'],
+                values: [{
+                  value: 80000
+                }, {
+                  value: 120000
+                }]
             }
         }
         case 'impressions': {
             return {
                 id: 'impressions',
-                tablet: 20000000,
-                smartphone: 30000000,
-                total: 50000000
-            }
+                total: 50000000,
+                colors: ['#ABDFE9', '#2E4955'],
+                values: [{
+                  value: 30000000
+                }, {
+                  value: 20000000
+                }]
+              }
         }
         case 'visits': {
             return {
                 id: 'visits',
-                tablet: 480000000,
-                smartphone: 120000000,
-                total: 600000000
+                value: 480000000,
+                total: 600000000,
+                colors: ['#D1C223', '#A75227'],
+                values: [{
+                  value: 120000000
+                }, {
+                  value: 480000000
+                }]
             }
         }
     }
